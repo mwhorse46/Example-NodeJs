15de341d8493a30b4001c7baeb052093f3e683af
mwhorse46 <needmefordev@gmail.com>
2020-02-03 19:28:29 +0100

    improved example

--- index.js
+++ index.js
@@ -1,45 +1,164 @@
 var sapphiredb = require('sapphiredb');
 var operators = require('rxjs/operators');
-var readline = require('readline');
-
+var inquirer = require('inquirer');
 var ws = require('ws');
 WebSocket = ws;
+
+var exampleCollection;
 
 var db = new sapphiredb.SapphireDb({
     serverBaseUrl: 'sapphiredb-demo.azurewebsites.net',
     useSsl: true
 });
 
-var exampleCollection = db.collection('basic.examples');
-var examples$ = exampleCollection.values();
-
-var examplesSubscription = examples$.subscribe(function (examples) {
-    console.log('\x1b[32m%s\x1b[0m', 'Examples:');
-    console.table(examples);
-});
-var newContentQuestion = function () {
-    var reader = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    reader.question('New content: (stop to stop adding entries)', function (content) {
-        reader.close();
-
-        if (content === 'stop') {
-            examplesSubscription.unsubscribe();
-            return;
+async function selectAction() {
+    var nextAction = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What do you want to do?',
+            choices: ['Add value', 'Update value', 'Remove value', 'Exit']
         }
+    ]);
 
-        exampleCollection.add({
-            content: content
-        });
+    if (nextAction.action === 'Add value') {
+        var newValue = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'content',
+                message: 'New content:'
+            }
+        ]);
 
-        newContentQuestion();
+        exampleCollection.add(newValue);
+    } else if (nextAction.action === 'Update value') {
+        var value = await inquirer.prompt([
+            {
+                type: 'number',
+                name: 'id',
+                message: 'Id:'
+            },
+            {
+                type: 'input',
+                name: 'content',
+                message: 'New content:'
+            }
+        ]);
+
+        exampleCollection.update(value);
+    } else if (nextAction.action === 'Remove value') {
+        var value = await inquirer.prompt([
+            {
+                type: 'number',
+                name: 'id',
+                message: 'Id:'
+            }
+        ]);
+
+        exampleCollection.remove(value);
+    } else {
+        process.exit();
+        return;
+    }
+
+    selectAction();
+}
+
+function basic() {
+    exampleCollection = db.collection('basic.examples');
+    var examples$ = exampleCollection.values();
+    var examplesSubscription = examples$.subscribe(function (examples) {
+        console.clear();
+        console.table(examples);
     });
-};
 
-newContentQuestion();
+    selectAction();
+}
 
+var messageCollection;
 
+async function chatMain(currentUser, chatPartner) {
+    var newValue = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'content',
+            message: 'New message:'
+        }
+    ]);
+    messageCollection.add({
+        content: newValue.content,
+        ownerId: currentUser.id,
+        receiverId: chatPartner.id
+    });
+    chatMain(currentUser, chatPartner);
+}
 
+async function chat() {
+    var users = await db.collection('chat.users').values().pipe(operators.take(1)).toPromise();
+
+    var currentUser = (await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'user',
+            message: 'Select a username:',
+            choices: users.map(function (value) {
+                value.value = JSON.parse(JSON.stringify(value));
+                value.name = value.username;
+                return value;
+            })
+        }
+    ])).user;
+
+    var chatPartner = (await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'user',
+            message: 'Select a username:',
+            choices: users.filter(function (user) {
+                return user.id !== currentUser.id;
+            }).map(function (value) {
+                value.value = JSON.parse(JSON.stringify(value));
+                value.name = value.username;
+                return value;
+            })
+        }
+    ])).user;
+
+    messageCollection = db.collection('chat.messages').where([
+        [['ownerId', '==', currentUser.id], 'and', ['receiverId', '==', chatPartner.id]],
+        'or',
+        [['ownerId', '==', chatPartner.id], 'and', ['receiverId', '==', currentUser.id]]
+    ]);
+
+    var messages$ = messageCollection.values();
+    var messagesSubscription = messages$.subscribe(function (messages) {
+        console.clear();
+        console.table(messages.map(function (value) {
+            return {
+                received: currentUser.id !== value.ownerId ? value.content : null,
+                sent: currentUser.id === value.ownerId ? value.content : null,
+                date: value.createdOn
+            };
+        }));
+    });
+
+    chatMain(currentUser, chatPartner);
+}
+
+async function main() {
+    var mode = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'mode',
+            message: 'What mode do you want to use?',
+            choices: ['Basic', 'Chat']
+        }
+    ]);
+
+    if (mode.mode === 'Basic') {
+        basic();
+    } else {
+        chat();
+    }
+}
+main();
